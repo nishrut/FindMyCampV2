b98bcf001cce3d272fcf7b565884f8781f6617c5
Nishrut Patel <76273871+nishrut@users.noreply.github.com>
2025-05-01 16:54:12 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from YELP CAMP!")
+})
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+})
